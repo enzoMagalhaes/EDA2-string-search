10ed6fabaeef0ebd789e9b71e48e31612fade7d4
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-12-01 10:41:26 -0300

    boyer moore fix

--- src/BoyerMoore.py
+++ src/BoyerMoore.py
@@ -1,50 +1,65 @@
 class BM:
-    def bad_character_table(pattern):
+    def generate_bad_char_table(pattern):
         table = {}
-        m = len(pattern)
+        pattern_length = len(pattern)
 
-        for i in range(m - 1):
-            table[pattern[i]] = m - 1 - i
-
-        return table
-
-    def good_suffix_table(pattern):
-        m = len(pattern)
-        table = [m] * m
-        j = m - 1
-
-        for i in range(m - 1, -1, -1):
-            if i == m - 1 or pattern[i] == pattern[j]:
-                table[j] = j - i + m - 1
-                j -= 1
-
-        for i in range(m - 1):
-            table[i] = max(table[i], j - i + m)
+        for i in range(pattern_length - 1):
+            table[pattern[i]] = pattern_length - 1 - i
 
         return table
+
+    def generate_good_suffix_table(pattern):
+        pattern_length = len(pattern)
+        suffixes = [0] * pattern_length
+        last_prefix_position = pattern_length
+
+        for i in range(pattern_length - 1, -1, -1):
+            if BM.is_suffix(pattern, i + 1):
+                last_prefix_position = i + 1
+            suffixes[i] = last_prefix_position - (pattern_length - 1 - i)
+
+        for i in range(pattern_length - 1):
+            j = BM.compute_prefix(pattern, i)
+            if i + j < pattern_length and pattern[i + j] != pattern[j]:
+                suffixes[pattern_length - 1 - j] = pattern_length - 1 - i + j
+
+        return suffixes
+
+    def is_suffix(pattern, i):
+        pattern_length = len(pattern)
+        suffix_length = pattern_length - i
+        suffix = pattern[i:pattern_length]
+        return suffix == pattern[0:suffix_length]
+
+    def compute_prefix(pattern, i):
+        j = 0
+        pattern_length = len(pattern)
+
+        while i + j < pattern_length and pattern[i + j] == pattern[j]:
+            j += 1
+
+        return j
 
     def search(text, pattern):
-        if not text or not pattern:
-            return -1
+        bad_char_table = BM.generate_bad_char_table(pattern)
+        good_suffix_table = BM.generate_good_suffix_table(pattern)
 
-        n, m = len(text), len(pattern)
-        bad_char_table = BM.bad_character_table(pattern)
-        good_suffix_table_values = BM.good_suffix_table(pattern)
+        text_length = len(text)
+        pattern_length = len(pattern)
+        i = 0
 
-        i = m - 1
-        while i < n:
-            j = m - 1
+        while i <= text_length - pattern_length:
+            j = pattern_length - 1
 
-            while j >= 0 and text[i] == pattern[j]:
-                i -= 1
+            while j >= 0 and pattern[j] == text[i + j]:
                 j -= 1
 
-            if j == -1:
-                return i + 1  # Pattern found
+            if j < 0:
+                return i  # Match found
 
-            bad_char = bad_char_table.get(text[i], m)
-            good_suffix = good_suffix_table_values[j]
+            bad_char = bad_char_table.get(text[i + j], pattern_length)
+            good_suffix = good_suffix_table[j]
 
             i += max(bad_char, good_suffix)
 
-        return -1  # Pattern not found
+        return -1  # No match found
